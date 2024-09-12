678db9607dfebcc21d171352e5e6ca0c5bc42f59
praisetech1 <praisesamasunga04@gmail.com>
2024-09-12 11:16:46 +0200

    fourth

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,16 +10,7 @@ const IBMPlex = IBM_Plex_Sans({
   weight: ["400", "500", "600", "700"],
   variable: "--foot-ibm-plex",
 });
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "AutoGenix",
